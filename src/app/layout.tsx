0d4f8194cf635713d639dee3f201ef738bec42cf
Kacper <33208083+demondobf@users.noreply.github.com>
2023-01-21 17:44:07 +0100

    Cleanup initial template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
-import './globals.css'
+import { Architects_Daughter } from '@next/font/google';
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const font = Architects_Daughter({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-brand',
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body className={`${font.variable} font-sans`}>{children}</body>
     </html>
-  )
+  );
 }
